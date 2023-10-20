279482ccf5b0087abc8b30174a396c01831ed4e9
Your Name <you@example.com>
2023-10-20 13:35:00 +0300

    fix-product

--- src/pages/menu/menu.tsx
+++ src/pages/menu/menu.tsx
@@ -1,11 +1,34 @@
 import ProductCard from '../../components/product-card/product-card';
+import { useEffect, useState } from "react";
 
 
 const Menu = () => {
+
+	const [state, setState] = useState({})
+
+	const test = async () => {
+		const e = await fetch('https://8yfn3y-3000.preview.csb.app/menu')
+			.then(res => res.json())
+			.then(item => setState(item[0]))
+			.catch(err => {
+				return [];
+			});
+		return e;
+	};
+
+
+
+	useEffect(() => {
+		test()
+
+	},[])
+
+console.log(state)
+
 	return (
 		<div>
 			<h1 style={{color: 'white'}}>Menu</h1>
-
+			{state && <img src={state.imgTop} alt=""/>}
 		</div>
 	);
 };
