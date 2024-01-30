96614556f36c792835b3b0d87d8feeb9ade64f99
Your Name <you@example.com>
2024-01-31 00:04:09 +0300

    Version 1.3

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,3 @@
-export const dataUrl = 'http://localhost:3001'
+export const dataUrl = 'https://midas.antonkarpukhin.com/api'
 export const JWT_STATE = 'userData';
 export const dataErrorMessage = 'Не удалось загрузить данные, попробуйте обновить страницу.';
